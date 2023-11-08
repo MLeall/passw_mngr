22bd014a05dfdfe636362b676484d7933b2944fe
Matheus Leal Leone <matheusleal.leone@gmail.com>
2023-11-08 11:41:44 -0500

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+from time import sleep
 
 class PasswordManager():
     pw_bank = []  # Need to be a database with verified access. SQLite should be good.
@@ -31,9 +31,24 @@ class PasswordManager():
         service = self.email_mng()
         pw = self.generate_pw()
         login = [service, pw]
-        self.pw_bank.append(login)        
+        self.pw_bank.append(login)
+        return login
 
 if __name__ == '__main__':
     final = PasswordManager()
-    final.create_login()
-    print(final.pw_bank)
+    while True:
+        while True:
+            try:
+                q = int(input('1 - Show logins\n2 - Create login\n3 - Exit\n\n> '))
+                break
+            except ValueError as e:
+                f'{e} - Value must be 1 or 2'
+
+        if q == 1:
+            print(final.pw_bank, '\n')
+        elif q == 2:
+            print(final.create_login(), '\n')
+        else:
+            print('Quiting...')
+            sleep(1.5)
+            break
